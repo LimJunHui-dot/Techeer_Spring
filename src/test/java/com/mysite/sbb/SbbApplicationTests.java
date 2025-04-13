796d813810ee660f59b8854a77a408899c0845a5
LimJunHui <junhuilim320@gmail.com>
2025-04-13 20:50:47 +0900

    점프 투 스프링 부트: 스프링 부트의 기본 기능 익히기

--- src/test/java/com/mysite/sbb/SbbApplicationTests.java
+++ src/test/java/com/mysite/sbb/SbbApplicationTests.java
@@ -1,12 +1,15 @@
 package com.mysite.sbb;
 
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
 
-import java.time.LocalDateTime;
+import java.util.List;
 import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @SpringBootTest
 class SbbApplicationTests {
@@ -14,22 +17,18 @@ class SbbApplicationTests {
 	@Autowired
 	private QuestionRepository questionRepository;
 
-	@Autowired
-	private AnswerRepository answerRepository;
-
+	@Transactional
 	@Test
 	void testJpa(){
 
 		Optional<Question> oq = this.questionRepository.findById(2);
-		Assertions.assertTrue(oq.isPresent());
+		assertTrue(oq.isPresent());
 		Question q = oq.get();
 
-		Answer a = new Answer();
-		a.setContent("네 자동으로 생성됩니다.");
-		a.setQuestion(q);
-		a.setCreateDate(LocalDateTime.now());
-		this.answerRepository.save(a);
+		List<Answer> answerList = q.getAnswerList();
 
+		assertEquals(1, answerList.size());
+		assertEquals("네 자동으로 생성됩니다.", answerList.get(0).getContent());
 		/*assertEquals(7, this.questionRepository.count());
 		Optional<Question> oq = this.questionRepository.findById(1);
 		assertTrue(oq.isPresent());
