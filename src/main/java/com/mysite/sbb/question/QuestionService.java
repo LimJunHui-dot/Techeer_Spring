9c6a68de5ee1ab79de37335b5bdcffdc19740cab
LimJunHui <junhuilim320@gmail.com>
2025-04-28 11:16:08 +0900

    상세 페이지 만들기

--- src/main/java/com/mysite/sbb/question/QuestionService.java
+++ src/main/java/com/mysite/sbb/question/QuestionService.java
@@ -1,9 +1,11 @@
 package com.mysite.sbb.question;
 
 import lombok.RequiredArgsConstructor;
+import com.mysite.sbb.DataNotFoundException;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @RequiredArgsConstructor
 @Service
@@ -16,6 +18,11 @@ public class QuestionService {
     }
 
     public Question getQuestion(Integer id){
-        
+        Optional<Question> question = this.questionRepository.findById(id);
+        if(question.isPresent()){
+            return question.get();
+        }else{
+            throw new DataNotFoundException("question not found");
+        }
     }
 }
